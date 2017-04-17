0d575fc79f5701677d50e34b599cf41654f1cc36
Innozium <zium349@daum.net>
2017-04-17 18:39:03 +0900

    170417 Pm 0635 Update JW
    170417 Pm 0635 Update JW
    // SimpleDepthView1 추가

--- Assets/Script/SimpleDepthView.cs
+++ Assets/Script/SimpleDepthView.cs
@@ -3,6 +3,9 @@ using Windows.Kinect;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using OpenCVForUnity;
+
 public class SimpleDepthView : MonoBehaviour
 {
     //Kinect V2 FPS 30FPS *3(Color) 30FPS(Depth) , 심도 취득 범위 0.5 ~ 8.0M 
@@ -45,7 +48,7 @@ public class SimpleDepthView : MonoBehaviour
         gameObject.transform.localScale = new Vector3(scale * depthFrameDesc.Width / depthFrameDesc.Height, scale, 1.0f);
 
 
-        StartCoroutine(CheckTest2());
+        StartCoroutine(CheckTest());
     }
 
     void Update()
@@ -57,19 +60,18 @@ public class SimpleDepthView : MonoBehaviour
     void updateTexture()
     {
         // get new depth data from DepthSourceManager.
+       
         ushort[] rawdata = depthSourceManagerScript.GetData();
-        ushort ave;
-        //print(ave);
 
-        for (int r = 0; r < f_DepthMapHeight; r += 1) //위에서 아래로
+        for (int r = 0; r < f_DepthMapHeight; r += 2) //위에서 아래로
         {
-            for (int c = 0; c < f_DepthMapWidth; c += 1)//오른쪽에서 왼쪽으로 
+            for (int c = 0; c < f_DepthMapWidth; c += 2)//오른쪽에서 왼쪽으로 
             {
 
                 ushort value = rawdata[r * f_DepthMapWidth + c];
                 depthBitmapBuffer[(r * f_DepthMapWidth + c) * 4 + 1] =
                     (byte)((DEPTHMAP_UNIT_CM_MIN < value) && (value < DEPTHMAP_UNIT_CM_MAX) ? 255 : 0); // G // COMMON
-                depthBitmapBuffer[(r * f_DepthMapWidth + c) * 4 + 1] =
+                depthBitmapBuffer[(r * f_DepthMapWidth + c) * 4 + 3] =
                     (byte)((DEPTHMAP_UNIT_CM_MIN < value) && (value < DEPTHMAP_UNIT_CM_MAX) ? 255 : 0); // G // COMMON
             }
         }
@@ -77,28 +79,10 @@ public class SimpleDepthView : MonoBehaviour
         // make texture from byte array
         texture.LoadRawTextureData(depthBitmapBuffer);
         texture.Apply();
+       
     }
 
     IEnumerator CheckTest()
-    {
-        yield return new WaitForFixedUpdate();
-
-        int count = 0;
-
-        while (true)
-        {
-            BottomCheck();
-
-            yield return new WaitForFixedUpdate();
-            if (count > 2) break;
-
-            count++;
-
-        }
-    }
-
-
-    IEnumerator CheckTest2()
     {
         // get new depth data from DepthSourceManager.
         ushort[] rawdata = depthSourceManagerScript.GetData();
@@ -150,9 +134,15 @@ public class SimpleDepthView : MonoBehaviour
             if (count > 2) break;
 
         }
+        print(check1Max);
+        print(check2Max);
 
-        sum = (ushort)(check1Max + check2Max);
-        average = (ushort)(sum / 2);
+
+        sum = (check1Max + check2Max);
+        average = (sum / 2);
+
+        print(average);
+
 
         do
         {
@@ -185,6 +175,7 @@ public class SimpleDepthView : MonoBehaviour
         average2 = (sum / (check3.Count + 1));
         print(sum);
         print(check3.Count);
+        print(average2);
 
         do
         {
@@ -218,66 +209,13 @@ public class SimpleDepthView : MonoBehaviour
         average3 = (sum / (check4.Count + 1));
         print(sum);
         print(check4.Count);
-
-
-
-
-        print(check1Max);
-        print(check2Max);
-        print(average);
-        print(average2);
         print(average3);
-
 
 
         DEPTHMAP_UNIT_CM_MAX = (ushort)average3;
         DEPTHMAP_UNIT_CM_MIN = DEPTHMAP_UNIT_CM_MAX - 100;
         //DEPTHMAP_UNIT_CM_MAX -= 50;
     }
-
-    void BottomCheck()
-    {
-        // get new depth data from DepthSourceManager.
-        ushort[] rawdata = depthSourceManagerScript.GetData();
-
-        bool check = false;
-        DEPTHMAP_UNIT_CM_MAX = 4500;
-
-        
-
-        while (!check)
-        {
-            int count = 0;
-
-            for (int i = 0; i < rawdata.Length; i++)
-            {
-                //적외선이 충돌되는 물체가 없다면 최대값? 일테니
-                //충된 객체가 많은 범위를 찾기위해
-                //도달한 길이가 Max값보다 작은게 많다면
-                //카운트 증가
-                if (rawdata[i] <= DEPTHMAP_UNIT_CM_MAX)
-                {
-                    count++;
-                }
-            }
-
-            //카운트가 쏘아진 적외선 갯수의 일정개수 보다 많다면 아직 최소길이가 아니다.
-            //그렇다면 최대길이를 감소.
-            //카운트가 더 적다면 그 길이로 부딪힌 영역이 많다는 소리이니...
-            //그것을 바닥으로 해보자.
-            if (count >= rawdata.Length - (rawdata.Length / 4)) DEPTHMAP_UNIT_CM_MAX -= 5;
-            else check = true;
-
-            if(check) print(count);
-            if (DEPTHMAP_UNIT_CM_MAX <= 500) break;
-        }
-
-        DEPTHMAP_UNIT_CM_MIN = DEPTHMAP_UNIT_CM_MAX - 100;
-        DEPTHMAP_UNIT_CM_MAX -= 50;
-        //print(count);
-
-    }
-
 
 }
 
---
+++ Assets/Script/TileTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVForUnity;
+using Windows.Kinect;
+
+public class TileTest : MonoBehaviour {
+
+    private bool is_foot = false;
+
+    private Transform tr;
+
+    private int count = 0;
+
+    // Use this for initialization
+    void Start() {
+
+        tr = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        is_foot = GetPosition(tr.position);
+
+       
+    }
+
+    bool GetPosition(Vector3 pos)
+    {
+        
+        RaycastHit hit;
+
+        Ray ray = new Ray(tr.position, Vector3.forward);
+
+        Debug.DrawRay(ray.origin, ray.direction, Color.red, 1000);
+        if (!Physics.Raycast(ray, out hit))
+            return false;
+
+        Renderer rend = hit.transform.GetComponent<MeshRenderer>();
+        MeshCollider meshCollider = (MeshCollider)hit.collider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+            return false;
+        //Debug.Log(hit.transform.name);
+        Texture2D tex = (Texture2D)rend.material.mainTexture;
+        Vector2 pixelUV = hit.textureCoord;
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+
+        count = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if ((pixelUV.x + i < tex.width && pixelUV.x + i > 0)
+                    && (pixelUV.y + j < tex.height && pixelUV.y + j > 0))
+                {
+                    if (tex.GetPixel((int)pixelUV.x + i, (int)pixelUV.y + j) == Color.green)
+                    {
+                        count++;
+                    }
+                }
+
+            }
+        }
+
+        //print(tex.GetPixel((int)pixelUV.x, (int)pixelUV.y));
+
+        if (count > 20) return true;
+        else return false;
+        
+    }
+}
